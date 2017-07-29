d0c3efee0eeeb17ec70539a69e088d5571148302
chaitanyavp <chaitanya1998@gmail.com>
2017-07-29 17:36:03 -0400

    Converted file into a Class.

--- visualizer.py
+++ visualizer.py
@@ -1,30 +1,38 @@
 import pygame
 
 
-def run_display():
+class Visualizer:
     """
-    Initializes a display and then makes it listen for events.
-    :return:
-    :rtype:
+    Takes in file size data and visually represents it in a Treemap using
+    pygame.
     """
-    print()
-    pygame.init()
-    pygame.display.set_mode([800, 600])
-    event_listener()
 
+    def __init__(self):
+        pygame.init()
 
-def event_listener():
-    """
-    Continuously loops, waiting for user inputs.
+    def run_display(self):
+        """
+        Initializes a display and then makes it listen for events.
+        :return:
+        :rtype:
+        """
+        pygame.display.set_mode([800, 600])
+        self.event_listener()
 
-    return: None
-    """
-    continue_running = True
-    while continue_running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                continue_running = False
-    pygame.quit()
+    def event_listener(self):
+        """
+        Continuously loops, waiting for user inputs.
+
+        return: None
+        """
+        continue_running = True
+        while continue_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    continue_running = False
+        pygame.quit()
+
 
 if __name__ == "__main__":
-    run_display()
+    v = Visualizer()
+    v.run_display()
