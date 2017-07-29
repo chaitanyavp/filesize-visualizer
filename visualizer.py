c295fcb50580df942f8c658bdb9cfc3406c4dbc2
chaitanyavp <chaitanya1998@gmail.com>
2017-07-29 13:57:53 -0400

    Added 2 methods, one to initialize and one to handle events

--- visualizer.py
+++ visualizer.py
@@ -1,1 +1,30 @@
 import pygame
+
+
+def run_display():
+    """
+    Initializes a display and then makes it listen for events.
+    :return:
+    :rtype:
+    """
+    print()
+    pygame.init()
+    pygame.display.set_mode([800, 600])
+    event_listener()
+
+
+def event_listener():
+    """
+    Continuously loops, waiting for user inputs.
+
+    return: None
+    """
+    continue_running = True
+    while continue_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continue_running = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    run_display()
