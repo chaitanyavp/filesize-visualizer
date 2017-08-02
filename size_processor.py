26ad59218db5a5a64441d4258c0f7308b6360993
chaitanyavp <chaitanya1998@gmail.com>
2017-08-01 21:24:05 -0400

    Partially implemented a method to calculate rectangles to draw

--- size_processor.py
+++ size_processor.py
@@ -2,7 +2,6 @@ import os
 
 
 class SizeProcessor:
-
     def __init__(self, folder_path, divider="/"):
         self._divider = divider
         self._file_sizes = {}
@@ -18,7 +17,7 @@ class SizeProcessor:
                 folder_size += self._file_sizes[item_path]
             else:
                 item_size = os.path.getsize(item_path)
-                subtrees.append(item_path)
+                subtrees.append((item_path,[]))
                 self._file_sizes[item_path] = item_size
                 folder_size += item_size
 
@@ -32,9 +31,30 @@ class SizeProcessor:
     def get_file_sizes(self):
         return self._file_sizes
 
+    def calculate_rectangles(self, x, y, width, height, file_tree, into_column):
+
+        # Loop through subtrees of
+        rect_list = []
+        for item in file_tree[1]:
+            # if item is not a directory, add its proportion
+            # otherwise recurse.
+            print(item[0])
+            proportion = self._file_sizes[item[0]] / \
+                         self._file_sizes[file_tree[0]]
+            if not os.path.isdir(item[0]):
+                rect_list.append((x, y, proportion * width, height))
+            else:
+                rect_list += self.calculate_rectangles(x, y, proportion * width,
+                                                       height, item,
+                                                       not into_column)
+            x += proportion*width
+
+        return rect_list
+
 
 if __name__ == "__main__":
     # Test by adding a filepath below:
     s = SizeProcessor("Add filepath here")
     print(s.get_file_tree())
     print(list(s.get_file_sizes().items()))
+    print(s.calculate_rectangles(0, 0, True))
