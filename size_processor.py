42a58989e5e6fed9aa88ed375967ddcf4e49d9f2
Ruize Liu <ruize.liu@mail.utoronto.ca>
2017-08-02 20:51:12 -0400

    Added footer code and pygame launches now. Also added a few docstrings.

--- size_processor.py
+++ size_processor.py
@@ -2,10 +2,22 @@ import os
 
 
 class SizeProcessor:
+    """
+    A Processing Object containing the necessary methods to calculate file sizes,
+    directory subfolders, and even positions of a clicked directory (on screen)
+
+    @type divider: char
+        A directory path dividing symbol.
+    @type _file_sizes: List<int>
+        A list of file sizes for a directory's subfolders.
+    @type _file_tree: Tuple<string, List<Tuple>>
+        A directory, compriesed of its path and its subfolders.
+    """
     def __init__(self, folder_path, divider="/"):
         self._divider = divider
         self._file_sizes = {}
         self._file_tree = self._add_subtrees(folder_path)
+        self._decimal_addon = 0.0
 
     def _add_subtrees(self, folder_path):
         subtrees = []
@@ -17,22 +29,32 @@ class SizeProcessor:
                 folder_size += self._file_sizes[item_path]
             else:
                 item_size = os.path.getsize(item_path)
-                subtrees.append((item_path,[]))
+                subtrees.append((item_path, []))
                 self._file_sizes[item_path] = item_size
                 folder_size += item_size
 
         self._file_sizes[folder_path] = folder_size
 
-        return (folder_path, subtrees)
+        return folder_path, subtrees
 
     def get_file_tree(self):
+        """
+        Gets the path for this directory (I think...?)
+        """
         return self._file_tree
 
     def get_file_sizes(self):
+        """
+        Gets the list of subfolder sizes for this directory.
+        """
         return self._file_sizes
 
     def calculate_rectangles(self, x, y, width, height, file_tree, into_column):
-
+        """
+        A loop that uses the original/root directory, and creates rectangular tuples
+        out of itself and its subfolders recursively.
+        It later returns the list to be visualized on screen.
+        """
         # Loop through subtrees of
         rect_list = []
         for item in file_tree[1]:
@@ -41,13 +63,27 @@ class SizeProcessor:
             print(item[0])
             proportion = self._file_sizes[item[0]] / \
                          self._file_sizes[file_tree[0]]
+
+            # Unimplemented code for decimal rounding
+            # Accounts for decimal roundoffs
+            # whole_proportion = proportion[:]
+            # dec_proportion = 0.0
+            # if proportion % 1 != 0:
+            #     dec_proportion = proportion - int(proportion)
+            #     whole_proportion -= dec_proportion
+
             if not os.path.isdir(item[0]):
                 rect_list.append((x, y, proportion * width, height))
             else:
                 rect_list += self.calculate_rectangles(x, y, proportion * width,
                                                        height, item,
                                                        not into_column)
+
             x += proportion*width
+
+            # Unimplemented code for decimal rounding
+            # x += whole_proportion*width
+            # self._decimal_addon += dec_proportion
 
         return rect_list
 
--- visualizer.py
+++ visualizer.py
@@ -29,15 +29,15 @@ class Visualizer:
         self._screen_width = 800
         self._screen_height = 600
 
+        self._file_display = pygame.display.set_mode(
+            [self._screen_width, self._screen_height])
+
     def run_display(self):
         """
         Initializes a display and then makes it listen for events.
 
         return: None
         """
-        file_display = pygame.display.set_mode(
-            [self._screen_width, self._screen_height])
-
         print(self._size_processor.calculate_rectangles(
                 0, 0, self._screen_width, self._screen_height,
                 self._size_processor.get_file_tree(), True))
@@ -45,14 +45,39 @@ class Visualizer:
         for rectangle in self._size_processor.calculate_rectangles(
                 0, 0, self._screen_width, self._screen_height,
                 self._size_processor.get_file_tree(), True):
+            curr_colour = self.generate_random_color()
+            pygame.draw.rect(self._file_display, curr_colour, rectangle)
 
-            pygame.draw.rect(file_display, self.generate_random_color(), rectangle)
+
+        # Code to generate a footer and starter text
+        #
+        # Code for generating a random coloured footer + complementory coloured text
+        # For now it's scrapped because some combinations look kind of bad
+        #
+        # footer_colour = self.generate_random_color()
+        # pygame.draw.rect(file_display, (footer_colour[0], footer_colour[1], footer_colour[2]),
+        #                  (0, self._screen_height - 25, self._screen_width, 25))
+        # footer_label = self._update_text("Please click on a block to show its details!", footer_colour)
+
+        # Current code
+        self._update_text("Please click on a block to show its details!")
+
+        pygame.display.flip()
         self.event_listener()
 
     def _update_text(self, new_text):
         """
         Stub for the update text method.
         """
+        pygame.draw.rect(self._file_display, (127, 255, 0),
+                         (0, self._screen_height - 25, self._screen_width, 25))
+        new_font = pygame.font.SysFont("Cambria", 12)
+        new_label = new_font.render(new_text, 1, (148, 0, 211))
+        self._file_display.blit(new_label, (10, self._screen_height - 20))
+
+        # If you ever want to try random footer colour generation again (code below)
+        # new_label = new_font.render(new_text, 1, (255 - footer_colours[0], 255 - footer_colours[1],
+        #                                           255 - footer_colours[2]))
 
     def event_listener(self):
         """
@@ -70,10 +95,7 @@ class Visualizer:
                     display_text = self.process_left_click(
                         pygame.mouse.get_pos())
                     self._update_text(display_text)
-                elif event.type == pygame.KEYUP:
-                    self.process_up_key()
-                elif event.type == pygame.KEYDOWN:
-                    self.process_down_key()
+                    pygame.display.flip()
         pygame.quit()
 
     def process_left_click(self, pos):
@@ -87,28 +109,23 @@ class Visualizer:
             A set of x/y coordinates for the mouse's current position.
         return: None
         """
-        text_to_display = ""
+        text_to_display = "Oh wait right now it doesn't work..."
         # selected_file = find_file(pos[0], pos[1], self._screen_width,
         #                           self._screen_height)
         # text_to_display = selected_file.path_name + " " + selected_file.file_size + " " + \
         #                   selected_file.subfolders.size + " subfolders"
         return text_to_display
 
-    def process_up_key(self):
-        """
-        Stub for the up key event.
-        """
-
-    def process_down_key(self):
-        """
-        Stub for the down key event.
-        """
-
     def generate_random_color(self):
+        """
+        Uses the python random module to generate a random colour.
+        return: A random colour tuple (of 3 ints).
+        rtype: int,int,int
+        """
         red_value = random.randint(0, 255)
         green_value = random.randint(0, 255)
         blue_value = random.randint(0, 255)
-        return (red_value, green_value, blue_value)
+        return red_value, green_value, blue_value
 
 
 if __name__ == "__main__":
